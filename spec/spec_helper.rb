e8d09149e518f3517567cf1adff0aeb4bc9f1212
Piotr Murach <pmurach@gmail.com>
2012-08-26 13:49:32 +0100

    Setup rspec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,10 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
+require 'communist'
 
 RSpec.configure do |config|
   config.order = :rand
+  config.color_enabled = true
+  config.tty = true
 end
