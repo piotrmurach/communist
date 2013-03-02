6d0e3776f73b2806ddaa15400a16cc6bf85170ed
Piotr Murach <pmurach@gmail.com>
2013-03-02 12:48:59 +0000

    Update tests.

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -4,28 +4,27 @@ describe Communist::Server do
 
   let(:app) { proc { |env| [200, {'Content-Length' => '13'}, "Hello Server!"] } }
 
-  context 'initialization' do
-    it 'sets application' do
-      server = Communist::Server.new(app)
-      server.app.should == app
-    end
+  subject(:server) { described_class.new(app).start }
 
-    it "sets port" do
-      server = Communist::Server.new(app).start
-      server.port.should == Communist::Server.ports[app.object_id]
-    end
-  end
+  after { server.stop }
+
+  its(:app) { should == app }
+
+  its(:host) { should == '127.0.0.1' }
+
+  its(:port) { should == described_class.ports[app.object_id] }
+
+  its(:responsive?) { should be_true }
 
   it "spins up a server" do
-    server = Communist::Server.new(app).start
     res = Net::HTTP.start(server.host, server.port) { |http| http.get('/') }
-    res.body.should include("Hello Server!")
+    expect(Communist.servers).to_not be_empty
+    expect(res.body).to eql "Hello Server!"
   end
 
   it "binds to specified host" do
     Communist.server_host = "0.0.0.0"
-    server = Communist::Server.new(app).start
-    server.host.should == "0.0.0.0"
+    expect(server.host).to eq "0.0.0.0"
     Communist.server_host = nil
   end
 
