2006ec62616edf04eb30483cd4c87c85ae0a35b0
Piotr Murach <pmurach@gmail.com>
2016-05-15 16:19:17 +0100

    Change specs setup.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,3 @@
-require 'rubygems'
-
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-
-require 'rspec'
 require 'communist'
 
 DEFAULT_TEST_HOST = '0.0.0.0'
@@ -22,9 +16,30 @@ module Helpers
 end
 
 RSpec.configure do |config|
-  config.order = :rand
-  config.color_enabled = true
-  config.tty = true
-
   config.include Helpers
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  # Limits the available syntax to the non-monkey patched syntax that is recommended.
+  config.disable_monkey_patching!
+
+  # This setting enables warnings. It's recommended, but in some cases may
+  # be too noisy due to issues in dependencies.
+  config.warnings = true
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.profile_examples = 2
+
+  config.order = :random
+
+  Kernel.srand config.seed
 end
